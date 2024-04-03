60f6941baba65e2fda19bd87383d533e2ddf44f3
rakshitvk19 <58843287+rakshithvk19@users.noreply.github.com>
2024-04-02 20:53:15 -0400

    refactored to next.js

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { WagmiWrapper } from "./WagmiWrapper";
+
+export default function Home() {
+  return (
+    <main>
+      <WagmiWrapper>
+        <div>Hello world</div>
+      </WagmiWrapper>
+    </main>
+  );
+}
