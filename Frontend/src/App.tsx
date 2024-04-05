b7ed000a05714297eb3e86970b61a574ebb2d261
rakshitvk19 <58843287+rakshithvk19@users.noreply.github.com>
2024-04-05 14:55:39 -0400

    refactored to vite

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <img src="https://gray-passive-chicken-829.mypinata.cloud/ipfs/Qmazb8vUP747ow3Di9QLQ3n5mzgWAeXP4RScy5oQbQyftY?pinataGatewayToken=n-oC6-uUw-e3PtBHio9Jexzq2pBjeuAsGA-3GQSkpjAuAELx8JGWHqaOSDVTz6B9"></img>
+    </div>
+  );
+}
+
+export default App;
