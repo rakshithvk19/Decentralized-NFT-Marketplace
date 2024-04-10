56cc0994edcdf0ddf0e0cd8d8b851a53d2de4337
vedantdalvi <dalvi.v@northeastern.edu>
2024-04-09 21:08:09 -0400

    Contract changes

--- Contracts/src/Marketplace.sol
+++ Contracts/src/Marketplace.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
 contract NFTMarketplace {
     struct Listing {
@@ -9,7 +11,14 @@ contract NFTMarketplace {
         uint256 price;
     }
 
+    struct TokenInfo {
+        uint256 tokenId;
+        uint256 price;
+        string tokenURI;
+    }
+
     mapping(address => mapping(uint256 => Listing)) public listings; // Mapping from token contract to token ID to Listing
+    mapping(address => uint256[]) private ownedTokens; // Mapping from NFT collection contract to array of owned token IDs
 
     function listNFT(
         address NFTCollectionContract,
@@ -22,6 +31,7 @@ contract NFTMarketplace {
             tokenId
         ); // Transfer the NFT to the marketplace contract
         listings[NFTCollectionContract][tokenId] = Listing(msg.sender, price);
+        ownedTokens[NFTCollectionContract].push(tokenId);
     }
 
     function buyNFT(
@@ -38,5 +48,37 @@ contract NFTMarketplace {
             tokenId
         ); // Transfer the NFT to the buyer
         payable(listing.seller).transfer(msg.value); // Send the payment to the seller
+
+        // Remove the token from the ownedTokens array
+        for (
+            uint256 i = 0;
+            i < ownedTokens[NFTCollectionContract].length;
+            i++
+        ) {
+            if (ownedTokens[NFTCollectionContract][i] == tokenId) {
+                ownedTokens[NFTCollectionContract][i] = ownedTokens[
+                    NFTCollectionContract
+                ][ownedTokens[NFTCollectionContract].length - 1];
+                ownedTokens[NFTCollectionContract].pop();
+                break;
+            }
+        }
+    }
+
+    function getOwnedNFTs(
+        address NFTCollectionContract
+    ) public view returns (TokenInfo[] memory) {
+        uint256[] memory tokenIds = ownedTokens[NFTCollectionContract];
+        TokenInfo[] memory tokenInfos = new TokenInfo[](tokenIds.length);
+
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            uint256 price = listings[NFTCollectionContract][tokenId].price;
+            string memory tokenURI = IERC721Metadata(NFTCollectionContract)
+                .tokenURI(tokenId);
+            tokenInfos[i] = TokenInfo(tokenId, price, tokenURI);
+        }
+
+        return tokenInfos;
     }
 }
