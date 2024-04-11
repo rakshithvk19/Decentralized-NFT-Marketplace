aa5038e1c9ea807605c44497a1e5365fc2c47e06
Rakshith R <58843287+rakshithvk19@users.noreply.github.com>
2024-04-11 16:51:57 -0400

    Rakshith changes (#15)
    * Mint NFT function working
    
    * cleaning code

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,11 @@
 import NFTUploader from "./components/NFTUploader";
 import Container from "@mui/material/Container";
+import { NFTList } from "./components/NFTList";
 import Box from "@mui/material/Box";
 import NFTGallery from "./components/NFTGallery";
 
 import { WagmiWrapper } from "./components/WagmiWrapper";
 import { MintNFT } from "./components/MintNFT";
-import { SendTransaction } from "./components/Send-Transaction";
-import TestWriteContract from "./components/TestWriteContract";
 
 const App: React.FC = () => {
   return (
